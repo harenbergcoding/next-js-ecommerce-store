32647ff08ce133545209cec054900a3cdf2c98b8
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-10-06 16:04:28 +0200

    Add databaseCommands.sql

---
+++ databaseCommands.sql
@@ -0,0 +1,18 @@
+-- used SQL queries
+
+CREATE TABLE nfts (
+id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name varchar (50) NOT NULL,
+type varchar (50) NOT NULL,
+description varchar (100) NOT NULL
+)
+
+
+INSERT INTO nfts (
+(id, name, type, description)
+VALUES
+('#1', 'Dragonfly', 1799, Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.),
+('#2', 'Squirrel', 2499, Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.),
+('#3', 'Panda', 1699, Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.),
+('#4', 'Rhino', 699, Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.)
+)
--- databaseExample.mjs
+++ databaseExample.mjs
@@ -3,9 +3,7 @@ import postgres from 'postgres';
 
 config();
 
-console.log('config()', config());
-
 const sql = postgres();
 // 'postgresql://nftdatabaseuser:nftdatabasepassword@localhost:5432/nftdatabase',
 
-console.log('nfts', await sql`SELECT * FROM nfts;`);
+// console.log('nfts', await sql`SELECT * FROM nfts;`);

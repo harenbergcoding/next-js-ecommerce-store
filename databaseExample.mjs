739ad13a3a6b7dc4402373e3df48aa1444aab54f
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-10-06 15:26:25 +0200

    First Database Setup

---
+++ databaseExample.mjs
@@ -0,0 +1,9 @@
+import { config } from 'dotenv-safe';
+import postgres from 'postgres';
+
+config();
+
+const sql = postgres();
+// 'postgresql://nftdatabaseuser:nftdatabasepassword@localhost:5432/nftdatabase',
+
+console.log('nfts', await sql`SELECT * FROM nfts;`);

265c9f803cf14aff1b1f0a2e356489dbf794dd17
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-10-15 16:07:33 +0200

    add migrations

---
+++ migrations/create-table-nfts.js
@@ -0,0 +1,17 @@
+exports.up = async (sql) => {
+  await sql`
+    CREATE TABLE nfts (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      name varchar(40) NOT NULL,
+      type varchar(40) NOT NULL,
+      price integer,
+      decription varchar (200) NOT NULL,
+    )
+  `;
+};
+
+exports.down = async (sql) => {
+  await sql`
+    DROP TABLE nfts
+  `;
+};
